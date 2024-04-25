d060122750c647309f92382f6fd5bfaa2fd6a053
tim004 <16382637+tim004@users.noreply.github.com>
2024-04-25 13:41:46 +0200

    Add changes to catalog, nodes and pipelines

--- src/eit_epsilon/pipelines/scheduling_engine/pipeline.py
+++ src/eit_epsilon/pipelines/scheduling_engine/pipeline.py
@@ -1,23 +1,40 @@
 from kedro.pipeline import Pipeline, node, pipeline
-import datetime
 
-from .nodes import order_generator,get_starting_jobs, fill_schedule
-
+from .nodes import load_jobs, schedule_earliest_due_date
+# from .nodes import get_starting_jobs, fill_schedule
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=get_starting_jobs,
-                inputs=["orders", "params:scheduling_options"],
-                outputs=["starting_schedule", "remaining_orders"],
-                name="initialize_schedule",
+                func=load_jobs,
+                inputs=["croom_open_orders", "params:scheduling_options"],
+                outputs="job_list",
+                name="load_jobs",
             ),
             node(
-                func=fill_schedule,
-                inputs=["remaining_orders", "starting_schedule"],
+                func=schedule_earliest_due_date,
+                inputs=["job_list", "params:scheduling_options"],
                 outputs="final_schedule",
-                name="finalize_schedule",
+                name="schedule_earliest_due_date",
             ),
         ]
     )
+
+# def create_pipeline(**kwargs) -> Pipeline:
+#     return pipeline(
+#         [
+#             node(
+#                 func=get_starting_jobs,
+#                 inputs=["orders", "params:scheduling_options"],
+#                 outputs=["starting_schedule", "remaining_orders"],
+#                 name="initialize_schedule",
+#             ),
+#             node(
+#                 func=fill_schedule,
+#                 inputs=["remaining_orders", "starting_schedule"],
+#                 outputs="final_schedule",
+#                 name="finalize_schedule",
+#             )
+#         ]
+#     )
