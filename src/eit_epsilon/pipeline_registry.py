1db417f636895e6c5eb9e22fc871e49b78b3b333
IonutBarbu <i.barbu@brightcape.nl>
2024-05-06 10:17:09 +0200

    first commit files to run day ahead pipeline

---
+++ src/eit_epsilon/pipeline_registry.py
@@ -0,0 +1,153 @@
+"""Project pipelines."""
+from typing import Dict
+
+import eit_epsilon.pipelines.energy_forecasting.data_funnel as data_funnel
+import eit_epsilon.pipelines.energy_forecasting.preprocessing as preprocessing
+import eit_epsilon.pipelines.energy_forecasting.modeling as modeling
+import eit_epsilon.pipelines.energy_forecasting.exploration as exploration
+from kedro.pipeline import Pipeline, pipeline
+
+import eit_epsilon.pipelines.energy_forecasting.data_extraction.energy_production as extract_energy_production
+# from pipelines.energy_forecasting.data_extraction import energy_production as extract_energy_production
+import eit_epsilon.pipelines.energy_forecasting.data_extraction.weather as extract_weather
+
+from pathlib import Path
+
+from kedro.config import OmegaConfigLoader
+from kedro.framework.project import settings
+
+conf_path = str(Path.cwd() / settings.CONF_SOURCE)
+conf_loader = OmegaConfigLoader(conf_source=conf_path)
+parameters = conf_loader["parameters"]
+
+data_to_use = parameters["energy_production_data_to_use"]
+
+def register_pipelines() -> Dict[str, Pipeline]:
+    """Register the project's pipelines.
+
+    Returns:
+        A mapping from pipeline names to ``Pipeline`` objects.
+    """
+    # pipelines = find_pipelines()
+
+
+    energy_production_pipeline = (
+            extract_energy_production.create_pipeline() +
+            preprocessing.create_pipeline_preprocess_energy_production()
+    )
+
+    weather_stations_pipeline = (
+            extract_weather.create_pipeline_weather_stations() +
+            data_funnel.create_pipeline_linking_weather_stations()
+    )
+
+    weather_measurements_pipeline = (
+            extract_weather.create_pipeline_measurements() +
+            preprocessing.create_pipeline_preprocess_weather_data()
+    )
+
+    modeling_training_pipeline=(modeling.create_pipeline_training_model())
+
+    weather_forecasts_pipeline = (
+            extract_weather.create_pipeline_forecasting() +
+            preprocessing.create_pipeline_preprocess_weather_forecasts()
+    )
+
+    modeling_testing_pipeline = (modeling.create_pipeline_testing_model())
+
+    weather_forecasts_testing_pipeline = pipeline(
+        pipe=weather_forecasts_pipeline,
+        parameters={
+            "params:start_date": "params:testing.start_date",
+            "params:stop_date": "params:testing.stop_date",
+            'params:locations': 'params:testing.locations',
+            'params:pilot_locations_coordinates': 'params:pilot_locations_coordinates'
+        },
+        outputs={
+            "weather_forecasts": "hourly_weather_forecasts_for_testing",
+            "closest_coordinates": "closest_coordinates_testing",
+            "preprocessed_weather_forecasts": "preprocessed_weather_forecasts_for_testing"
+        },
+        namespace='weather_forecasts_testing'
+    )
+
+    modeling_forecasting_pipeline = (modeling.create_pipeline_forecasting_model())
+    exploration_pipeline = (exploration.create_pipeline())
+    data_funnel_forecasting_pipeline = data_funnel.create_pipeline_weather_forecasts()
+
+    # Day ahead forecasting pipelines
+    # Model training day ahead
+    data_funnel_training_and_testing_pipeline = (
+        data_funnel.create_pipeline_merging_production_weather_data()
+    )
+    day_ahead_forecasting_pipeline = pipeline(
+        pipe=weather_forecasts_pipeline + data_funnel_forecasting_pipeline + modeling_forecasting_pipeline,
+        parameters={
+            "params:pilot_locations_coordinates": "params:pilot_locations_coordinates",
+            "params:meta_data": "params:meta_data",
+            "params:start_date": "params:day_ahead_forecasting_pipeline.forecasting.start_date",
+            "params:stop_date": "params:day_ahead_forecasting_pipeline.forecasting.stop_date",
+            'params:locations': 'params:day_ahead_forecasting_pipeline.forecasting.locations',
+            'params:feature_engineering_selection': 'params:day_ahead_model_training.feature_engineering_selection',
+            'params:data_interpolation': 'params:day_ahead_model_training.data_interpolation',
+        },
+        inputs={
+            "trained_model": "trained_model_day_ahead"
+        },
+        outputs={
+            "forecasting_data": "forecasting_day_ahead_data",
+            "forecasting_predictions": "forecasting_day_ahead_predictions",
+            "closest_coordinates": "closest_coordinates_day_ahead_forecasting",
+            "selected_models": "selected_day_ahead_models",
+
+        },
+        namespace='day_ahead_forecasting_pipeline'
+    )
+
+    # Model forecasting day ahead
+    day_ahead_model_training_pipeline = pipeline(
+        pipe=energy_production_pipeline + weather_stations_pipeline +
+             weather_measurements_pipeline + data_funnel_training_and_testing_pipeline +
+             weather_forecasts_testing_pipeline + modeling_training_pipeline +
+             modeling_testing_pipeline + exploration_pipeline,
+        parameters={
+            'params:knmi.weather_stations.start': 'params:knmi.weather_stations.start',
+            'params:knmi.weather_stations.end': 'params:knmi.weather_stations.end',
+            'params:meta_data': 'params:meta_data',
+            'params:pilot_locations_coordinates': 'params:pilot_locations_coordinates',
+            'params:knmi.hourly_measurements.variables': 'params:knmi.hourly_measurements.variables',
+            'params:weather_forecasts_testing.data_interpolation': 'params:day_ahead_model_training.data_interpolation',
+            'params:energy_production_data_to_use': 'params:energy_production_data_to_use'
+        },
+        inputs={
+            'data_to_use': data_to_use,
+        },
+        outputs={
+            "trained_model": 'trained_model_day_ahead',
+            "training_data": 'training_day_ahead_data',
+            "testing_data": 'testing_day_ahead_data',
+            "permuted_feature_importance_training": 'permuted_feature_importance_training_day_ahead',
+            "preprocessed_energy_production_data": "preprocessed_energy_production_data",
+            "solar_energy_production_per_location": "solar_energy_production_per_location",
+            "performance_testing_using_measurements": "performance_day_ahead_testing_using_measurements",
+            "performance_testing_using_forecasts": "performance_day_ahead_testing_using_forecasts",
+            'weather_stations': 'weather_stations',
+            'nearest_weather_stations': 'nearest_weather_stations',
+            'unique_weather_stations': 'unique_weather_stations',
+            'weather_measurements': 'weather_measurements',
+        },
+        namespace='day_ahead_model_training'
+    )
+
+
+    pipelines = {
+        "__default__": day_ahead_model_training_pipeline + day_ahead_forecasting_pipeline,
+        'modeling_training_day_ahead': day_ahead_model_training_pipeline,
+        'forecasting_day_ahead': day_ahead_forecasting_pipeline,
+        'day_ahead': day_ahead_model_training_pipeline + day_ahead_forecasting_pipeline,
+    }
+
+
+
+    # pipelines["__default__"] = sum(pipelines.values())
+    return pipelines
