66cd7c0c73fa813ad6e9f8561f8b58ac5688391a
tim004 <16382637+tim004@users.noreply.github.com>
2024-05-14 09:49:30 +0200

    Remove nodes that were commented out

--- src/eit_epsilon/pipelines/scheduling_engine/pipeline.py
+++ src/eit_epsilon/pipelines/scheduling_engine/pipeline.py
@@ -31,21 +31,3 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
         ]
     )
-
-# def create_pipeline(**kwargs) -> Pipeline:
-#     return pipeline(
-#         [
-#             node(
-#                 func=get_starting_jobs,
-#                 inputs=["orders", "params:scheduling_options"],
-#                 outputs=["starting_schedule", "remaining_orders"],
-#                 name="initialize_schedule",
-#             ),
-#             node(
-#                 func=fill_schedule,
-#                 inputs=["remaining_orders", "starting_schedule"],
-#                 outputs="final_schedule",
-#                 name="finalize_schedule",
-#             )
-#         ]
-#     )
