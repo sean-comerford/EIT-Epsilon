d4650ede5447820a6d9bdaf2afb3fcebbc66d398
tim004 <16382637+tim004@users.noreply.github.com>
2024-05-23 15:20:27 +0200

    Temporary changes to test different gantt chart settings

--- src/eit_epsilon/pipelines/scheduling_engine/pipeline.py
+++ src/eit_epsilon/pipelines/scheduling_engine/pipeline.py
@@ -5,22 +5,22 @@ from .nodes import load_jobs, schedule_earliest_due_date, create_chart, save_cha
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-            node(
-                func=load_jobs,
-                inputs=["croom_open_orders", "params:scheduling_options"],
-                outputs="job_list",
-                name="load_jobs",
-            ),
-            node(
-                func=schedule_earliest_due_date,
-                inputs=["job_list", "params:scheduling_options"],
-                outputs="final_schedule",
-                name="schedule_earliest_due_date",
-            ),
+            # node(
+            #     func=load_jobs,
+            #     inputs=["croom_open_orders", "params:scheduling_options"],
+            #     outputs="job_list",
+            #     name="load_jobs",
+            # ),
+            # node(
+            #     func=schedule_earliest_due_date,
+            #     inputs=["job_list", "params:scheduling_options"],
+            #     outputs="final_schedule",
+            #     name="schedule_earliest_due_date",
+            # ),
             node(
                 func=create_chart,
                 inputs=["final_schedule", "params:visualization_options"],
-                outputs="gantt_chart",
+                outputs="gantt_chart_json",
                 name="schedule_chart",
             ),
             node(
