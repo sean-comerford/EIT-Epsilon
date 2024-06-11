cf3d5e51d45d6de62402f185f2898176624a06d2
JeanLucOudshoorn <j.oudshoorn@brightcape.nl>
2024-06-11 16:18:27 +0200

    Clean, functional JobShop v1.0

--- src/eit_epsilon/pipelines/scheduling_engine/nodes.py
+++ src/eit_epsilon/pipelines/scheduling_engine/nodes.py
@@ -1,58 +1,154 @@
 import os
 from pathlib import Path
 import datetime
-
 import pandas as pd
 import re
+import logging
 from pandas.api.types import is_string_dtype
 import plotly
+from typing import List, Dict, Tuple
 
+# Instantiate logger
+logger = logging.getLogger(__name__)
 
 class Job:
-    def filter_in_scope_op1(self, data):
-        # Add your preprocessing logic here
+    def filter_in_scope_op1(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Filters the data to include only in-scope operations for OP 1.
+
+        Args:
+            data (pd.DataFrame): The input data.
+
+        Returns:
+            pd.DataFrame: The filtered data.
+        """
+        # Debug statement
+        logger.info(f"Total order data: {data.shape}")
+
+        # Apply the filter
         inscope_data = data[(data['Part Description'].str.contains('OP 1') |
                             data['Part Description'].str.contains('ATT ')) &
                             (~data['On Hold?']) &
                             (~data['Part Description'].str.contains('OP 2'))]
 
+        # Debug statement
+        logger.info(f"In-scope data for OP 1: {inscope_data.shape}")
+
         return inscope_data
 
-    def extract_info(self, data):
-        # Extract type, size, and orientation using.assign
+    def extract_info(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Extracts type, size, and orientation from the part description.
+
+        Args:
+            data (pd.DataFrame): The input data.
+
+        Returns:
+            pd.DataFrame: The data with extracted information.
+        """
         data = data.assign(
             Type=lambda x: x['Part Description'].apply(
                 lambda y: 'CR' if 'CR' in y else ('PS' if 'PS' in y else '')),
             Size=lambda x: x['Part Description'].apply(
                 lambda y: re.search(r'Sz (\d+N?)', y).group(1) if re.search(r'Sz (\d+N?)', y) else ''),
             Orientation=lambda x: x['Part Description'].apply(
-                lambda y: 'LEFT' if 'LEFT' in y.upper() else ('RIGHT' if 'RIGHT' in y.upper() else ''))
+                lambda y: 'LEFT' if 'LEFT' in y.upper() else ('RIGHT' if 'RIGHT' in y.upper() else '')),
+            Cementless=lambda x: x['Part Description'].apply(
+                lambda y: 'CLS' if 'CLS' in y.upper() else 'C')
         )
+
+        # Debug statement
+        if data[['Type', 'Size', 'Orientation']].isna().sum().sum() > 0:
+            logger.warning(f"Data with extracted information: {data[['Type', 'Size', 'Orientation']].isna().sum()}")
+        else:
+            logger.info(f"No missing values in Type, Size, and Orientation columns")
 
         return data
 
-    def check_part_id_consistency(self, data):
-        grouped = data.groupby('Part ID')[['Type', 'Size', 'Orientation']].nunique()
-        assert not (grouped > 1).any().any(), "Part ID should be unique for every combination of Type, Size, and Orientation"
+    def check_part_id_consistency(self, data: pd.DataFrame) -> None:
+        """
+        Checks the consistency of Part IDs.
 
-    def create_jobs_op1(self, data):
-        # Seven tasks per job in OP 1
-        J = [[1, 2, 3, 4, 5, 6, 7] for _ in range(len(data))]
+        Args:
+            data (pd.DataFrame): The input data.
+
+        Raises:
+            LoggerError: If Part ID is not unique for every combination of Type, Size, and Orientation.
+        """
+        grouped = data.groupby('Part ID')[['Type', 'Size', 'Orientation']].nunique()
+
+        if (grouped > 1).any().any():
+            logger.error("[bold red blink]Part ID not unique for every combination of Type, Size, and Orientation[/]",
+                         extra={"markup": True})
+        else:
+            logger.info(f"Part ID consistency check passed")
+
+
+    def create_jobs_op1(self, data: pd.DataFrame) -> List[List[int]]:
+        """
+        Creates jobs representation for the GA, defined as J.
+        Cemented products do not have to go through manual prep in OP 1.
+
+        Args:
+            data (pd.DataFrame): The input data.
+
+        Returns:
+            List[List[int]]: The list of jobs.
+        """
+        # Find proportion of cementless products
+        cementless_count = data[data['Cementless'] == 'CLS'].shape[0]
+        total_products = data.shape[0]
+
+        cementless_percentage = (cementless_count / total_products) * 100
+        logger.info(f"Proportion of cementless products: {cementless_percentage:.1f}%")
+
+        # Populate J
+        J = [[1, 2, 3, 4, 5, 6, 7] if cementless == 'CLS' else [1, 2, 3, 6, 7] for cementless in data['Cementless']]
+
+        if not len(J) == len(data):
+            logger.error("[bold red blink]J is not of the same length as processed orders![/]",
+                         extra={"markup": True})
+
+        # Debug statement
+        logger.info(f"Snippet of Jobs for OP 1: {J[:2]}")
 
         return J
 
 
 class Shop:
+    def create_machines(self, machine_qty_dict: Dict[str, int]) -> List[int]:
+        """
+        Creates a list of machines based on the quantity dictionary.
 
-    def create_machines(self, machine_qty_dict):
+        Args:
+            machine_qty_dict (Dict[str, int]): The dictionary of machine quantities.
+
+        Returns:
+            List[int]: The list of machines.
+        """
         total_machines = sum(machine_qty_dict.values())
-
-        # Create representation of M
         M = list(range(1, total_machines + 1))
+
+        # Debug statement
+        logger.info(f"Machine list (M): {M}")
 
         return M
 
-    def get_compatibility(self, J, task_to_machines):
+    def get_compatibility(self, J: List[List[int]], task_to_machines: Dict[int, List[int]]) -> List[List[List[int]]]:
+        """
+        Gets the compatibility of tasks to machines.
+
+        Example:
+            compat = [[[1], [2, 3], [4, 5]],  -- Job 1, task 1 is only compatible with machine 1
+                    [[1, 2],[2, 3],[4, 5]]]  -- Job 2, task 1 is compatible with machine 1 and 2
+
+        Args:
+            J (List[List[int]]): The list of jobs.
+            task_to_machines (Dict[int, List[int]]): The dictionary mapping tasks to machines.
+
+        Returns:
+            List[List[List[int]]]: The compatibility list.
+        """
         compat = []
         for job_tasks in J:
             job_compat = []
@@ -63,89 +159,150 @@ class Shop:
                     raise ValueError("Invalid task number!")
                 job_compat.append(machines)
             compat.append(job_compat)
+
+        # Debug statement
+        logger.info(f"Snippet of compatability matrix: {compat[0]}")
+
         return compat
 
-    def preprocess_cycle_times(self, cycle_times):
+    def preprocess_cycle_times(self, cycle_times: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Preprocesses the cycle times:
+            1.) Remove empty rows and columns
+            2.) Create new index starting from 1
+            3.) Reduce column names to only the size
+            4.) Fill the missing values in final inspection with 1 minute
+            5.) Split data for cruciate retaining and posterior stabilizing
 
-        # Set the first row as the headers
+        Args:
+            cycle_times (pd.DataFrame): The cycle times data.
+
+        Returns:
+            Tuple[pd.DataFrame, pd.DataFrame]: The preprocessed PS and CR cycle times.
+        """
         cycle_times.columns = cycle_times.iloc[1]
-
-        # Drop the first two rows and first column
         cycle_times = cycle_times.iloc[2:, 1:]
-
-        # Create new index starting from 1
         cycle_times.index = range(1, len(cycle_times) + 1)
 
-        def extract_number(s):
-            """ Extracts valid size from column name"""
+        def extract_number(s: str) -> str:
             match = re.search(r'\d+N?', s)
             return match.group(0) if match else s
 
-        # Apply size extraction
         cycle_times.columns = [extract_number(col) for col in cycle_times.columns]
+        cycle_times.loc[7] = cycle_times.loc[7].fillna(1)
+        ps_times, cr_times = cycle_times.iloc[:, :18], cycle_times.iloc[:, 18:]
 
-        # Fill times for final inspection
-        cycle_times.loc[7].fillna(1, inplace=True)
-
-        # Split cycle times for PS and CR into separate dataframes
-        ps_times, cr_times = cycle_times.iloc[:, :19], cycle_times.iloc[:, 19:]
+        # Debug statement
+        logger.info(f"PS times dim.: {ps_times.shape}, CR times dim.: {cr_times.shape}")
 
         return ps_times, cr_times
 
-    def get_duration_matrix(self, J, inscope_orders, cr_times, ps_times):
-        dur = []  # Initialize the duration matrix
-        for i, job in enumerate(J):  # Iterate over each job
-            job_dur = []  # Initialize the duration list for this job
-            for task in job:  # Iterate over each task in this job
-                # Determine whether to use cr_times or ps_times
+    def get_duration_matrix(self, J: List[List[int]], inscope_orders: pd.DataFrame, cr_times: pd.DataFrame, ps_times: pd.DataFrame) -> List[List[float]]:
+        """
+        Gets the duration matrix for the jobs.
+
+        Example:
+             dur = [[3, 2, 2],  -- Job 1; task 1, 2, 3 will take 3, 2, 2 minutes respectively
+                    [3, 3, 3]]  -- Job 2; task 1, 2, 3 will take 3, 3, 3 minutes respectively
+
+        Args:
+            J (List[List[int]]): The list of jobs.
+            inscope_orders (pd.DataFrame): The in-scope orders.
+            cr_times (pd.DataFrame): The CR cycle times.
+            ps_times (pd.DataFrame): The PS cycle times.
+
+        Returns:
+            List[List[float]]: The duration matrix.
+        """
+        dur = []
+        for i, job in enumerate(J):
+            job_dur = []
+            for task in job:
                 times = cr_times if inscope_orders.iloc[i]['Type'] == 'CR' else ps_times
-                # Look up the duration using the task number and size
-                duration = round(times.loc[task, inscope_orders.iloc[i]['Size']] * inscope_orders.iloc[i]['Order Qty'],
-                                 1)
+                duration = round(times.loc[task, inscope_orders.iloc[i]['Size']] * inscope_orders.iloc[i]['Order Qty'], 1)
                 job_dur.append(duration)
             dur.append(job_dur)
+
+        # Debug statement
+        logger.info(f"Snippet of duration: {dur[0]}")
+
         return dur
 
-    def get_due_date(self, inscope_orders, date='2024-03-18'):
-        # Initialize empty list of due dates
+    def get_due_date(self, inscope_orders: pd.DataFrame, date: str = '2024-03-18') -> List[int]:
+        """
+        Gets the due dates for the in-scope orders.
+
+        Args:
+            inscope_orders (pd.DataFrame): The in-scope orders.
+            date (str): The reference date.
+
+        Returns:
+            List[int]: The list of due dates in working minutes.
+        """
         due = []
-
         for due_date in inscope_orders['Due Date ']:
-            # Check if date is later than due_date
             if pd.Timestamp(date) > due_date:
-                # If so, generate range in opposite direction and multiply by -1
-                working_days = -len(pd.bdate_range(due_date, date)) * 480  # 480 minutes in an 8 hour working day
+                working_days = -len(pd.bdate_range(due_date, date)) * 480
             else:
-                # Otherwise, generate range as before
                 working_days = len(pd.bdate_range(date, due_date)) * 480
-
-            # append to list
             due.append(working_days)
+
+        # Debug statement
+        logger.info(f"Snippet of due: {due[:2]}")
 
         return due
 
 
 class JobShop(Job, Shop):
-    def preprocess_orders(self, croom_open_orders):
-        # Add your preprocessing logic here
+    def preprocess_orders(self, croom_open_orders: pd.DataFrame) -> pd.DataFrame:
+        """
+        Preprocesses the open orders.
+
+        Args:
+            croom_open_orders (pd.DataFrame): The open orders.
+
+        Returns:
+            pd.DataFrame: The preprocessed orders.
+        """
         inscope_data = (
             self.filter_in_scope_op1(croom_open_orders)
                 .pipe(self.extract_info)
-                .pipe(self.check_part_id_consistency)
         )
+        self.check_part_id_consistency(inscope_data)
 
         return inscope_data
 
-    def build_ga_representation(self, inscope_data, machine_qty_dict, cycle_times, task_to_machines):
-        # Create correct representation of required variables for GA
-        J = self.create_jobs_op1(inscope_data)
+    def build_ga_representation(self, croom_processed_orders: pd.DataFrame, cr_cycle_times: pd.DataFrame, ps_cycle_times: pd.DataFrame,
+                                machine_qty_dict: Dict[str, int], task_to_machines: Dict[int, List[int]]) -> Dict[str, any]:
+        """
+        Builds the GA representation:
+            J: List[List[int]]: The list of jobs, each job is a list of tasks.
+            M: List[int]: The list of machines.
+            compat: List[List[List[int]]]: The compatibility list.
+            dur: List[List[float]]: The duration matrix.
+            due: List[int]: The list of due dates in working minutes.
+
+        Args:
+            croom_processed_orders (pd.DataFrame): The processed orders.
+            cr_cycle_times (pd.DataFrame): The CR cycle times.
+            ps_cycle_times (pd.DataFrame): The PS cycle times.
+            machine_qty_dict (Dict[str, int]): The machine quantity dictionary.
+            task_to_machines (Dict[int, List[int]]): The task to machines dictionary.
+
+        Returns:
+            Dict[str, any]: The GA representation.
+        """
+        J = self.create_jobs_op1(croom_processed_orders)
         M = self.create_machines(machine_qty_dict)
         compat = self.get_compatibility(J, task_to_machines)
-        ps_times, cr_times = self.preprocess_cycle_times(cycle_times)
-        dur = self.get_duration_matrix(J, inscope_data, cr_times, ps_times)
-        due = self.get_due_date(inscope_data)
+        dur = self.get_duration_matrix(J, croom_processed_orders, cr_cycle_times, ps_cycle_times)
+        due = self.get_due_date(croom_processed_orders)
 
-        # Put all variables in the input dictionary
+        def is_nested_list_of_numbers(lst):
+            if isinstance(lst, list):
+                return all(is_nested_list_of_numbers(item) if isinstance(item, list) else isinstance(item, (int, float)) for item in lst)
+            return False
+
         input_repr_dict = {
             'J': J,
             'M': M,
@@ -154,14 +311,33 @@ class JobShop(Job, Shop):
             'due': due,
         }
 
+        # Check if J, M, compat, dur, and due are (nested) lists of integers or floats
+        for var_name, var in input_repr_dict.items():
+            if not is_nested_list_of_numbers(var):
+                logger.error(
+                    f"[bold red blink]{var_name} is not a nested list of integers/floats: {var[0]}[/]",
+                    extra={"markup": True})
+
         return input_repr_dict
 
 
-def create_chart(self, schedule: pd.DataFrame):
+def create_chart(schedule: pd.DataFrame) -> None:
+    """
+    Creates a chart from the schedule.
+
+    Args:
+        schedule (pd.DataFrame): The schedule data.
+    """
     # Add your chart creation logic here
     pass
 
 
-def save_chart_to_html(self, gantt_chart):
+def save_chart_to_html(gantt_chart) -> None:
+    """
+    Saves the chart to an HTML file.
+
+    Args:
+        gantt_chart: The Gantt chart object.
+    """
     # Add your chart saving logic here
     pass
--- src/eit_epsilon/pipelines/scheduling_engine/pipeline.py
+++ src/eit_epsilon/pipelines/scheduling_engine/pipeline.py
@@ -1,36 +1,33 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from nodes import JobShop
+from .nodes import JobShop
 
 
 def create_pipeline(**kwargs) -> Pipeline:
 
+    # Instantiate a jobshop object
     jobshop = JobShop()
 
+    # Create a pipeline with the jobshop object
     pipeline = Pipeline([
         node(
-            func=jobshop.load_jobs,
-            inputs="croom_open_orders",
-            outputs="job_list",
-            name="load_jobs",
+            func=jobshop.preprocess_orders,
+            inputs='croom_open_orders',
+            outputs='croom_processed_orders',
+            name='preprocess_orders'
         ),
         node(
-            func=jobshop.schedule_earliest_due_date,
-            inputs='jobs_df',
-            outputs='schedule',
-            name='schedule_earliest_due_date_node'
+            func=jobshop.preprocess_cycle_times,
+            inputs='monza_cycle_times',
+            outputs=['ps_cycle_times', 'cr_cycle_times'],
+            name='preprocess_cycle_times'
         ),
         node(
-            func=jobshop.create_chart,
-            inputs='schedule',
-            outputs='chart',
-            name='create_chart_node'
-        ),
-        node(
-            func=jobshop.save_chart_to_html,
-            inputs='chart',
-            outputs=None,
-            name='save_chart_to_html_node'
+            func=jobshop.build_ga_representation,
+            inputs=['croom_processed_orders', 'cr_cycle_times', 'ps_cycle_times',
+                    'params:machine_qty_dict', 'params:task_to_machines'],
+            outputs='input_repr_dict',
+            name='build_ga_representation'
         ),
     ])
 
